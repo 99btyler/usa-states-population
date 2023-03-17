411249985f81a55e1e7e16bf1c3648a2f8fda513
99btyler <99tylerberinger@gmail.com>
2023-03-17 14:13:14 -0400

    Improved scraping, switched to csv file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 
+
+// Formatted for Wikipedia's url and "historical population" table
 const states = [
     "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
     "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana",
@@ -10,45 +12,52 @@ const states = [
     "Oklahoma", "Oregon", "Pennsylvania", "Rhode_Island", "South_Carolina", "South_Dakota", "Tennessee",
     "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
 ];
+const YEAR_EARLIEST = 1790;
+const YEAR_LATEST = 2020;
+const YEAR_INCREMENT = 10;
+
 
 (async () => {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    const data = {};
+    // Create csv_header
+    let csv_header = "state;";
+    for (var year = YEAR_EARLIEST; year <= YEAR_LATEST; year+=YEAR_INCREMENT) {
+        csv_header += `${year}(population);${year}(percentChange);`;
+    }
+    csv_header = `${csv_header.slice(0, -1)}\n`;
+    fs.writeFile("data.csv", csv_header, error => error ? console.log(error) : console.log("✓ Created csv_header"));
 
-    for (var i = 0; i < states.length; i++) {
+    // Create csv_data
+    for (const state in states) {
 
-        // Go to page
-        await page.goto(`https://en.wikipedia.org/wiki/${states[i]}`);
+        await page.goto(`https://en.wikipedia.org/wiki/${states[state]}`);
+        const rawdata = await page.$$eval(".us-census-pop tr", trs => trs.map(tr => tr.innerText));
 
-        // Get rawdata
-        const populationDataYears = await page.$$eval(".us-census-pop tr th", ths => ths.map(th => th.innerText)); // Starts at 4
-        const populationData = await page.$$eval(".us-census-pop tr td", tds => tds.map(td => td.innerText)); // Starts at 0, skips by 2
+        let csv_line = `${states[state]};`;
+        let expected_year = YEAR_EARLIEST;
+        for (var i = 1; i < rawdata.length-1; i++) {
 
-        // Organize rawdata
-        rawdata = []
+            const row = rawdata[i].split("\t"); // 0=year, 1=population, 2='', 3=percentChange
+            const state_year = row[0];
+            const state_population = row[1];
+            const state_percentChange = row[3];
 
-        yearIndex = 4;
-        for (var dataIndex = 0; dataIndex < populationData.length-1; dataIndex+=3) {
-            rawdata.push(
-                {
-                    year: populationDataYears[yearIndex],
-                    population: populationData[dataIndex],
-                    percentChange: populationData[dataIndex+2]
-                }
-            );
-            yearIndex += 1;
+            while (expected_year < parseInt(state_year)) {
+                csv_line += "—;—;";
+                expected_year += YEAR_INCREMENT;
+            }
+            csv_line += `${state_population};${state_percentChange};`;
+            expected_year += YEAR_INCREMENT;
+
         }
 
-        data[states[i]] = rawdata;
-        console.log(`✓${states[i]} data collected`);
+        csv_line = `${csv_line.slice(0, -1)}\n`;
+        fs.appendFile("data.csv", csv_line, error => error ? console.log(error) : console.log(`✓ Added csv_line ${states[state]}`));
 
     }
-
-    // Save data
-    fs.writeFile("data.json", JSON.stringify(data), error => console.log(error));
 
     await browser.close();
 
