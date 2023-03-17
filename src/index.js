576c1ac9901e5456805c94c69078898985e0d090
99btyler <99tylerberinger@gmail.com>
2023-03-17 14:23:35 -0400

    Fixed state names

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const puppeteer = require("puppeteer");
 // Formatted for Wikipedia's url and "historical population" table
 const states = [
     "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-    "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana",
+    "Delaware", "Florida", "Georgia_(U.S._state)", "Hawaii", "Idaho", "Illinois", "Indiana",
     "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
     "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",
-    "New_Hampshire", "New_Jersey", "New_Mexico", "New_York", "North_Carolina", "North_Dakota", "Ohio",
+    "New_Hampshire", "New_Jersey", "New_Mexico", "New_York_(state)", "North_Carolina", "North_Dakota", "Ohio",
     "Oklahoma", "Oregon", "Pennsylvania", "Rhode_Island", "South_Carolina", "South_Dakota", "Tennessee",
-    "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+    "Texas", "Utah", "Vermont", "Virginia", "Washington_(state)", "West Virginia", "Wisconsin", "Wyoming"
 ];
 const YEAR_EARLIEST = 1790;
 const YEAR_LATEST = 2020;
